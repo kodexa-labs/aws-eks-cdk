e5dc19fb0652cfdbce4e94b81fcfef60dbc92dd1
Philip Dodds <philip@kodexa.ai>
2021-04-28 13:44:18 -0400

    Trying to move this to work in the AWS Shell

--- aws-shell-go.sh
+++ aws-shell-go.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Setting up for a CDK installation for Kodexa"
-npm install aws-cdk
-pip3 install -r requirements.txt
+sudo npm install -g aws-cdk
+sudo pip3 install -r requirements.txt
 
 echo "Ok we have the basics installed, now run:\n"
-echo "./node_modules/bin/cdk deploy"
+echo "cdk deploy"
